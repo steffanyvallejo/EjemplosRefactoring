3dfe883571e719d8d56044a80695e3799a386ff3
steffanyvallejo <svallejo@espol.edu.ec>
2018-08-07 22:01:09 -0500

    Ejemplo metodo largo, aplicando extraccion de metodos

---
+++ LongMethod/src/longmethod/LongMethod.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package longmethod;
+import java.io.File;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Steffany Vallejo
+ */
+public class LongMethod extends Application {
+    
+    Boolean cambiarColor = true;
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        StackPane root = new StackPane();
+ 
+        Rectangle rectangulo = new Rectangle();
+        rectangulo.setX(50);
+        rectangulo.setY(50);
+        rectangulo.setWidth(100);
+        rectangulo.setHeight(70);
+        rectangulo.setOnMouseClicked(new EventHandler<MouseEvent>()
+        {
+            @Override
+            public void handle(MouseEvent t) {
+                if(cambiarColor){
+                    rectangulo.setFill(Color.BLACK);
+                    cambiarColor=false;
+                }
+                else{
+                    rectangulo.setFill(Color.AQUA);
+                    cambiarColor=true;
+                 
+                }
+            }
+        });
+        
+        root.getChildren().add(rectangulo);
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
